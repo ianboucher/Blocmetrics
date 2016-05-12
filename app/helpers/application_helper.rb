a5414d2eb2525b9549c1e46829af38e62955b063
Ian Boucher <i.a.boucher@gmail.com>
2016-05-12 16:36:54 +0100

    Customise styling of Devise form views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
 
+  # helper method for displaying gravatar image
   def gravatar_url(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, class: "gravatar")
   end
+
+  # helper method to display errors on forms
+  # def form_group_tag(errors, &block)
+  #   css_class = 'form-group'
+  #   css_class << ' has-error' if errors.any?
+  #   content_tag :div, capture(&block), class: css_class
+  # end
 end
