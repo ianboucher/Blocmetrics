b78d1e6f4e9c1ef30671e9de8714b2f6618a9322
Ian Boucher <i.a.boucher@gmail.com>
2016-05-12 13:06:16 +0100

    Add basic authenticaion with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # does this callback introduce some cross-linking?
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  # why can't this be handled in the relevant individual controllers?
+  # I tried using the commented out actions and 'before_actions' in the
+  # RegistrationsController with no luck...
+  def configure_permitted_params
+
+    added_attributes = [:username,
+                        :email,
+                        :password,
+                        :password_confirmation,
+                        :remember_me]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attributes)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gravatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    image_tag(gravatar_url, class: "gravatar")
+  end
 end
